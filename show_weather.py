a513804da2bcf0e4a8b88a6a984aa53cbe8dac69
Julia Bogdanova <49280929+AlexRikka@users.noreply.github.com>
2023-08-31 01:32:58 +0400

    upd: function returns text

--- show_weather.py
+++ show_weather.py
@@ -5,11 +5,11 @@ def get_weather(location):
     payload = {'n': '', 'M': '', 'T': '', 'q': '', 'lang': 'ru'}
     response = requests.get(f'https://wttr.in/{location}', params=payload)
     response.raise_for_status()
-    return response
+    return response.text
 
 
 if __name__ == '__main__':
     locations = ['Лондон', 'Шереметьево', 'Череповец']
     for location in locations:
         weather_report = get_weather(location)
-        print(weather_report.text)
+        print(weather_report)
